aa6c26d588fce09b524f149aadf227cc84ebdea0
tavargas9 <142061829+tavargas9@users.noreply.github.com>
2023-09-08 21:44:14 -0600

    Add initial JS

--- script.js
+++ script.js
@@ -2,12 +2,34 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+function generatePassword(length) {
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var randomPassword = "";
+
+for (var i = 0; i < length; i++) {
+  
+  var randomIndex = Math.floor(Math.random() * characters.length);
+  
+  
+  randomPassword += characters.charAt(randomIndex);
+}
+
+return randomPassword;
+}
+
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+  var userInput = prompt("Enter the desired password length:");
+  var passwordLength = parseInt(userInput);
+
+  if (!isNaN(passwordLength) && passwordLength > 8 && passwordLength < 128){
+    var password = generatePassword(passwordLength);
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  } else {
+    alert("Invalid input. Please enter a positive number between 8 and 128 for password length")
+  }
 }
 
 // Add event listener to generate button
